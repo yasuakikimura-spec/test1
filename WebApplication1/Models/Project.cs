2afb5549b79058e98827ceeef2c899447be7c2f9
Caitrin (Jitera) <ai_developers@jitera.com>
2025-08-02 10:15:26 +0000

    feat: add Package table and update Project model for 1-to-many relationship

--- WebApplication1/Models/Project.cs
+++ WebApplication1/Models/Project.cs
@@ -1,5 +1,6 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebApplication1.Models
@@ -10,11 +11,15 @@ namespace WebApplication1.Models
         public Guid ID { get; set; }
 
         [Required]
-        public string Name { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty; // string is non-nullable by default in C# 8.0+
 
         public string? Description { get; set; }
 
+
+
         [Timestamp]
-        public byte[]? RowVersion { get; set; }
+        public byte[]? RowVersion { get; set; } // byte[] is nullable
+
+        public List<Package> Packages { get; set; } = new();
     }
 }
